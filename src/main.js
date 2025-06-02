2e38b58ffc4edd2f054fca6f194fc26298602ea8
Felipe Andrade <felipe_saff1@hotmail.com>
2025-06-02 13:50:50 -0300

    ajuste responsitividade mobile and ajuste js header

--- src/main.js
+++ src/main.js
@@ -2,6 +2,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual < alturaHero){
+            ocultaElementosDoHeader();
+        }
+        else{
+            exibeElementosDoHeader();
+        }
+    })
+
+    
+    //secão de atração, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -14,12 +30,21 @@ document.addEventListener('DOMContentLoaded', function () {
             botao.target.classList.add('shows__tabs__button--is-active');
         });
     }
-
+    //seção de FAQ, accordion
     function escondeTodasAbas() {
         const tabsContainer = document.querySelectorAll('[data-tab-id]');
         for (let i = 0; i < tabsContainer.length; i++) {
             tabsContainer[i].classList.remove('shows__list--is-active');
         }
+    }
+
+    function ocultaElementosDoHeader(){
+        const header = document.querySelector('header');
+        header.classList.add('header--is-hidden');
+    }
+    function exibeElementosDoHeader(){
+        const header = document.querySelector('header');
+        header.classList.remove('header--is-hidden');
     }
 
     function removeBotaoAtivo() {
